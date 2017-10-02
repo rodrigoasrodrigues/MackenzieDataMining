6a84b5b80792695edd8bf197b8f64c6ecb926b8f
Rodrigo Rodrigues <serdasteclas@gmail.com>
2017-10-02 19:40:20 -0300

    Random Forest com Tidy Data (melhorou para 0.74)

---
+++ NYC_Taxi_Trip_Duration_RF_Kmeans_tidy.R
@@ -0,0 +1,44 @@
+#Importando as bibliotecas
+
+library(readr)
+library(data.table)
+library(lubridate)
+library(tidyverse)
+library(randomForest)
+
+source('PreparacaoDeDados.R')
+
+set.seed(20)
+
+NYCTripCluster <- kmeans(train[, 2:6], 2, nstart = 20)
+
+#verificando o cluster
+
+NYCTripCluster 
+
+#Criando a tabela para visualizar o resultado do Cluster com Kmeans
+
+base::table(NYCTripCluster$cluster,train$recording)
+
+#Preparando os dados para montar os modelos e calcular a distancia manhattan
+
+set.seed(412)
+
+
+
+#Criando um modelo com randomForest com ntree = 100 para fazer a predição da trip_duration com vendor_id + passenger_count + pickup_longitude + pickup_latitude 
+
+rf_trip_duration <- randomForest(trip_duration ~ vendor_id + passenger_count + recording + distance + traffic ,data = train,ntree = 100)
+
+#fazendo a predição do modelo para trip_duration
+
+rf_prediction <- predict(rf_trip_duration, test, type = "response") 
+
+#Preparando o arquivo de submissão em dataframe com a predição com id e trip_duration
+
+submission_file <- data.frame(id = test$id, trip_duration = rf_prediction)
+
+#Escrevendo o arquivo.csv para a submissão
+
+write.csv(submission_file, "randomForest_tidy.csv", row.names=F)
+
